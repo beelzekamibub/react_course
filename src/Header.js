ca5554842128c69f4f9da09f6fecfcc65052004d
Chaitanya Sharma <chaitanya.sharma2@incedoinc.com>
2023-02-04 15:35:50 +0530

    event handling

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,14 @@ export default function Header() {
     const headerStyle={
         backgroundColor:'mediumorchid', color:'white'
     };
+    const handleClick=(e)=>{
+      console.log(e);
+    }
   return (
-    <header style={headerStyle}>
-        <h1>groceries</h1>
+    // <header style={headerStyle}>
+    <header>
+        <h1 onDoubleClick={handleClick}>groceries</h1>
+        <button onClick={(e)=>{handleClick(e)}}> click it </button>
     </header>
   )
 }
