0cdc143dc3b18f4477d20320d53c047a8f56bc78
Chaitanya Sharma <chaitanya.sharma2@incedoinc.com>
2023-02-04 15:53:22 +0530

    use state hook

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Content() {
+    const [name, setName]=useState('a');
+    let names=['a','b','c'];
   return (
     <main>
-        <p> Content </p>
+        <p> Content : {name}</p>
+        <button onClick={()=>{setName(names[Math.floor(Math.random()*names.length)])}}> change name</button>
     </main>
   )
 }
