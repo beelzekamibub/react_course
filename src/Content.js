e7bc89d5e4ba81bafe76cc5dbb639fa7cd8fbd62
Chaitanya Sharma <chaitanya.sharma2@incedoinc.com>
2023-02-04 17:41:24 +0530

    keys list react icons local storage eventhandler on change

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
+import {FaTrashAlt} from 'react-icons/fa';
 
 export default function Content() {
-    const [name, setName]=useState('a');
-    let names=['a','b','c'];
-  return (
-    <main>
-        <p> Content : {name}</p>
-        <button onClick={()=>{setName(names[Math.floor(Math.random()*names.length)])}}> change name</button>
-    </main>
-  )
+
+    const [items, setItems]=useState([
+                                        {
+                                            id:1,
+                                            checked:false,
+                                            item: "cheese"
+                                        },
+                                        {
+                                            id:2,
+                                            checked:false,
+                                            item: "item 2"
+                                        },
+                                        {
+                                            id:3,
+                                            checked:false,
+                                            item: "item 3"
+                                        }
+                                    ]);
+    
+    const handleCheck=(id)=>{
+        const listitems=items.map((item) => item.id===id ? {...item,checked:!item.checked} :item );
+        setItems(listitems);
+        localStorage.setItem('shoppingList',JSON.stringify(listitems));
+
+    }
+    const handleDelete=(id)=>{
+        const listitems=items.filter((item)=>item.id!=id);
+        setItems(listitems);
+        localStorage.setItem('shoppingList',JSON.stringify(listitems));
+    }
+    return (
+        <main>
+            <ul>
+                {items.map((item)=>(
+                    <li className='item' key={item.id}>
+                        <input type='checkbox' onChange={()=>{handleCheck(item.id)}} checked={item.checked}></input>
+                        <label style={(item.checked)?{textDecoration:'line-through'}:null} onDoubleClick={()=>{handleCheck(item.id)}}>{item.item}</label>
+                       <FaTrashAlt onClick={()=>handleDelete(item.id)} role="button" tabIndex="0"></FaTrashAlt>
+                    </li>
+                    
+                ))}
+            </ul>
+        </main>
+    )
 }
